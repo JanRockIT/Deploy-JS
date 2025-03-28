37e81e0f7f08d5529208eb1db69261450e6f24ec
JanRockIT <programmieren001@gmail.com>
2025-03-28 14:43:46 +0000

    started firebase auth functions

--- api-deploy/firebase-auth/firebase-config.js
+++ api-deploy/firebase-auth/firebase-config.js
@@ -1,1 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0w71pT0DEXHdkQbATR9LW2KiMi2xkcz4",
+  authDomain: "wavify-official.firebaseapp.com",
+  databaseURL: "https://wavify-official-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wavify-official",
+  storageBucket: "wavify-official.firebasestorage.app",
+  messagingSenderId: "1011854459875",
+  appId: "1:1011854459875:web:32f135be5d374b951816d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
